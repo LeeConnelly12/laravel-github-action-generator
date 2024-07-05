630821153eea9b6ac721989443c08a590ff20ea2
Lee Connelly <lee@leeconnelly.dev>
2024-07-06 00:17:01 +0100

    Set container defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ import forms from '@tailwindcss/forms'
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1152px',
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
